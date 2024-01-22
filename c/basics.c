7e8dc8aad674446fd59c13d38de93bb1eb543e90
jessica <2027felker.jessica@fcboe.com>
2024-01-22 14:47:49 -0500

    c basics

---
+++ c/basics.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	printf("this is my c basics file\n");   // write an output statement
+	
+	int num;   // type name;
+	printf("please enter a number: ", num);   // prompt the user for a number
+	scanf("%i", &num);   // store user input in num
+	printf("your number was %i\n", num);
+
+	return 0;
+
+
+}
