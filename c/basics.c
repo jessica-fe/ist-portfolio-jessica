e9d87a3707cae3a4d7b88bec0235212102cc74d4
jessica <2027felker.jessica@fcboe.com>
2024-01-24 14:15:28 -0500

    age, gpa, letter grade

--- c/basics.c
+++ c/basics.c
@@ -6,6 +6,10 @@ int main()
 	printf("this is my c basics file\n");   // write an output statement
 	
 	int num;   // type name;
+		   // type name = value;
+		   // int is integers (pos and neg numbers)  -  int num = (integer)
+		   // float is decimals or fractions -  float average = (decimal)
+		   // char is singular letters or symbols (a b + or even a singular number)  -  char grade = '\c'
 	printf("please enter a number: ", num);   // prompt the user for a number
 	scanf("%i", &num);   // store user input in num
 	printf("your number was %i\n", num);
---
+++ c/scanf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+	int age;
+	float gpa;
+	char grade;
+
+	printf("What is your age? \n");
+	scanf("%i", &age);
+
+	printf("What is your gpa? \n");
+	scanf("%f", &gpa);
+
+	printf("What is your letter grade? \n");
+	scanf(" %c", &grade);
+
+	printf("%i, %f, %c \n", age, gpa, grade);
+
+}
+
